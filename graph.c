418bfe44d65f6e18e119c805483fba55c9699534
deepaec24-spec <deepa.ec24@builderscollege.edu.in>
2026-02-06 09:43:22 +0530

    add changes

---
+++ graph.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int A, B;
+
+  
+    scanf("%d", &N);
+
+    int adj[N][N];
+
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+   
+    scanf("%d %d", &A, &B);
+
+   
+    if (adj[A][B] == 1) {
+        printf("Connected");
+    } else {
+        printf("Not Connected");
+    }
+
+    return 0;
+}
